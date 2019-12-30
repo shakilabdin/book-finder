f177b86c657a74c59b8c7b3e17a229ec1fcce63e
warren mcdonald <warrenjmac@mail.com>
2019-12-30 17:40:53 -0500

    working on validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,29 @@ class Book < ApplicationRecord
   belongs_to :author
   has_many :reviews
   accepts_nested_attributes_for :author
+  validates :title, presence: true
+  validate :unique_book?
+
+  def self.titles
+    Book.all.map do |book|
+      book.title
+    end
+  end
+
+  def self.authors
+    Book.all.map do |book|
+      book.author
+    end
+  end
+
+  def unique_book?
+    if self.title && self.author && Book.titles.select do |title|
+        self.title ==  title 
+      end &&  Book.authors.select do |author|
+        self.author == author
+      end
+      self.errors.add(:title, "has been added already")
+    end
+  end
+
 end
